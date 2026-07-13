a7eddeff35e3b55847a7f53758cf67d1a5cafed2
eugenekoen <56178255+eugenekoen@users.noreply.github.com>
2026-07-13 14:34:23 +0200

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/OnBoarding-App/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
